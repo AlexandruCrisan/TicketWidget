a2268c85109ba2c1df677a595e7de0fc853243f6
AlexandruCrisan <calex2005cj@gmail.com>
2024-09-19 22:56:41 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import TicketWidget from "./components/TicketWidget";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+    return (
+        <div>
+            <TicketWidget
+                apiBaseUrl="https://localhost:5001/api"
+                apiKey={props.apiKey}
+            />
+        </div>
+    );
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const widgetDivs = document.querySelectorAll(".tockzy-ticket-widget");
+
+widgetDivs.forEach((div) => {
+    const root = ReactDOM.createRoot(div);
+    root.render(
+        <React.StrictMode>
+            <App
+                apiKey={div.dataset.apiKey}
+                apiBaseUrl={div.dataset.apiBaseUrl}
+            />
+        </React.StrictMode>
+    );
+    // ReactDOM.render(
+    //     <React.StrictMode>
+    //         <App
+    //             apiKey={div.dataset.apiKey}
+    //             apiBaseUrl={div.dataset.apiBaseUrl}
+    //         />
+    //     </React.StrictMode>,
+    //     div
+    // );
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
